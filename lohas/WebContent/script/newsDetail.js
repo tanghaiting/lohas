bbf791f143698ebdaf428f63663a2384bc23b047
tanghaiting <985663595@qq.com>
2017-10-26 08:43:35 +0800

    啊

--- lohas/WebContent/script/newsDetail.js
+++ lohas/WebContent/script/newsDetail.js
@@ -1,18 +1,19 @@
 var newsDetailApp = angular.module('newsDetailApp', ['ngCookies','constApp','ngSanitize']);
 	//注意：获取路径参数必须添加这个config
-	newsDetailApp.config(['$locationProvider', function($locationProvider) {  
-		$locationProvider.html5Mode(true);  
-	}]);
+//	newsDetailApp.config(['$locationProvider', function($locationProvider) {  
+//		$locationProvider.html5Mode(true);  
+//	}]);
 	newsDetailApp.controller('newsDetailController', ['$scope', '$http', '$const','$location', '$sce',function($scope, $http, $const, $location,$sce){
 		//获取路径参数
-		var id =  $location.search().id;
+//		var id =  $location.search().id;
+		var url = window.location.href; 
+		var id = url.substring("?").split("=")[1];
 		//新闻详细内容
 		$scope.newsDetail = null;
 		$scope.getNewsDetail = function(){
 			var url = "new/news/"+id;
 			var promise = $const.syncRequest(url, "GET", "json", "");
 			promise.then(function(data){
-				console.log(data);
 				if(data != null){
 					$scope.newsDetail = data;
 					$scope.newsDetail.content = $sce.trustAsHtml(data.content);
